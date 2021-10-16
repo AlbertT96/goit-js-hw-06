8bcd83bd71851e98dd311ce80c08f30679789c62
Albert Traka <albert.traka@gmail.com>
2021-10-16 12:09:23 +0200

    small upgrade

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+function createBoxes(amount) {
+  const div = document.createElement("div");
+  div.style.height = "30px";
+  div.style.width = "30px";
+  div.style.backgroundColor = "#000";
+  document.body.append(div);
+}
+ 
